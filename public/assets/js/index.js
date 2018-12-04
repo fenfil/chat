4e5f9cbced1152553392f3ef66d2433d8ec3d9dd
fenfil <vladimir.zaykovskiy@ya.ru>
2018-12-04 13:44:51 +0300

    default params added

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,6 @@
 document.getElementById('btn').addEventListener('click', function() {
-  let username = document.getElementById('name').value;
-  let room = document.getElementById('room').value;
-  
-  location.replace(`http://${location.host}/chat?room=${room}&username=${username}`); 
+  let username = document.getElementById('name').value || 'noname';
+  let room = document.getElementById('room').value || 'main';
+
+  location.replace(`http://${location.host}/chat?room=${room}&username=${username}`);
 });
